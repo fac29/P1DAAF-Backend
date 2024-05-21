5e6051af4ef9801a7b97e5c7b6dc52dd56866c39
Fearg <ferskav@gmail.com>
2024-05-21 10:28:06 +0100

    added filesystem

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
+import * as fs from 'fs'
 
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT || 3001
 
+const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+console.log(data)
+
 app.get('/', (req: Request, res: Response) => {
-	res.send('Expression + TypeScript Server')
+	res.send(data.questions)
 })
 
 app.listen(port, () => {
