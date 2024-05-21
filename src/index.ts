0eb1efe15fe0b5760c0bd1fa4a79832ead45e3a0
AlexVOiceover <alex@alexvoiceover.com>
2024-05-21 12:34:57 +0100

    cleaning code

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,56 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import * as fs from 'fs/promises';
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import * as fs from 'fs/promises'
 
-dotenv.config();
+dotenv.config()
 
 // Type definition for question
-type Difficulty = "easy" | "medium" | "hard";
+type Difficulty = 'easy' | 'medium' | 'hard'
 
 type Question = {
-    id: number;
-    category: string;
-    difficulty: Difficulty;
-    question: string;
-    options: Array<string>;
-    answer: string;
-    favourited: boolean; // This should be boolean instead of true
-    timestamp: Date;
-};
-
-type Questions = Array<Question>;
-type OuterQuestion = { questions: Questions }; // Adjusted to match the JSON structure
-
-const app: Express = express();
-const port = process.env.PORT || 3001;
-
-async function loadData(): Promise<OuterQuestion> {
-    const fileContent: string = await fs.readFile('data.json', 'utf8');
-    return JSON.parse(fileContent);
+	id: number
+	category: string
+	difficulty: Difficulty
+	question: string
+	options: Array<string>
+	answer: string
+	favourited: boolean // This should be boolean instead of true
+	timestamp: Date
 }
 
-let data: OuterQuestion;
+type Questions = Array<Question>
+type OuterQuestion = { questions: Questions } // Adjusted to match the JSON structure
 
-loadData().then((loadedData) => {
-    data = loadedData;
+const app: Express = express()
+const port = process.env.PORT || 3001
 
-    const questionsArray: Questions = data.questions; // Corrected to data.questions
-    console.log(questionsArray[0]);
+async function loadData(): Promise<OuterQuestion> {
+	const fileContent: string = await fs.readFile('data.json', 'utf8')
+	return JSON.parse(fileContent)
+}
 
-    app.get('/', (req: Request, res: Response) => {
-        res.json(data); // Use res.json to send data as a JSON object
-    });
+let data: OuterQuestion
 
-    app.listen(port, () => {
-        console.log(`[server]: Server is running at http://localhost:${port}`);
-    });
-}).catch((err) => {
-    console.error('Error loading data:', err);
-});
+app.listen(port, () => {
+	console.log(`[server]: Server is running at http://localhost:${port}`)
+})
+
+loadData()
+	.then((loadedData) => {
+
+		const questionsArray: Questions = loadedData.questions
+		console.log(questionsArray[0])
+
+		app.get('/', (req: Request, res: Response) => {
+			res.json(questionsArray) // Use res.json to send data as a JSON object
+		})
+
+		app.get('/first', (req: Request, res: Response) => {
+			res.json(questionsArray[0]) // Use res.json to send data as a JSON object
+		})
+
+		
+	})
+	.catch((err) => {
+		console.error('Error loading data:', err)
+	})
