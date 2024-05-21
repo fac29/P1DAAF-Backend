41b66cbf118d3994131989a99ce6b7dd1bbb834e
AlexVOiceover <alex@alexvoiceover.com>
2024-05-21 11:09:43 +0100

    Added type definitions

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,32 @@ import * as fs from 'fs'
 
 dotenv.config()
 
+// Type definition for question
+type difficulty = "easy" | "medium" | "hard"
+type question = {
+		id: number,
+		category: string,
+		difficulty: difficulty,
+		question: string,
+		options: Array<string>,
+		//Worth having an index instead?
+		answer: string,
+		favourited: true,
+		timestamp: Date
+}
+type questions = Array<question>
+
 const app: Express = express()
 const port = process.env.PORT || 3001
 
-const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+const data = JSON.parse(fs.readFileSync('data.json', 'utf8'))
 console.log(data)
+const questionsArray:questions = data.questions
+
+// console.log(questionsArray[0])
 
 app.get('/', (req: Request, res: Response) => {
-	res.send(data.questions)
+	res.send(data)
 })
 
 app.listen(port, () => {
