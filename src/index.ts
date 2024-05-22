944b021a4a75aff71e60147af3a767c38497fe90
AlexVOiceover <alex@alexvoiceover.com>
2024-05-22 16:55:10 +0100

    Excluded data.json from nodemon watch

--- src/index.ts
+++ src/index.ts
@@ -39,8 +39,6 @@ app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
-
-
 app.get("/", async (req: Request, res: Response) => {
   const filecontent = await loadData()
 	let Allquestions = filecontent.questions
